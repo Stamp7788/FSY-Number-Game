685a893f23b96c413974640264b70396909695fd
Gabriel Olsen <gabe.olsen778@gmail.com>
2024-05-28 10:23:03 -0500

    Gabe - Big Code

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
+const leaderboard = document.getElementById("leaderboard");
+const birdLogo = new Image("logo.png");
+const devLogo = new Image("devLogo.png");
+const dropdownMenu = document.getElementById("dropdownMenu");
+
+
+
 fetch("players.json")
 .then(function(response){
    return response.json();
 })
 .then(function(players){
-   let placeholder = document.querySelector("#data-output");
    let out = "";
+   let rank = 1;
+   let earlyIcon = "";
+   let devIcon= "";
+
    for(let player of players){
+      // Check for Early Participant status
+      if(player.earlyP == true) {
+         earlyIcon = birdLogo;
+      }
+      else {
+         earlyIcon = "";
+      }
+      // Check for Developer status
+      if(player.developer == true) {
+         devIcon = devLogo;
+      }
+      else {
+         devIcon = "";
+      }
       out += `
          <tr>
-            <td>${player.name}</td>
+            <td>${rank}</td>
+            <td>${devIcon}${earlyIcon}${player.name}</td>
             <td>${player.grade}</td>
             <td>${player.points}</td>
          </tr>
       `;
+      rank++;
    }
  
-   placeholder.innerHTML = out;
+   leaderboard.innerHTML = out;
 });
+
+
+function activateDropdownMenu() {
+   dropdownMenu.addEventListener('click', ()=> {
+
+   });
+}
