dfafc3a4c60d8e0a3f99d3bcfe04e896ec22570e
Jonathan Hill <jwhill1452@gmail.comm>
2024-05-28 18:34:20 -0500

    More key changes. The key is better. New icon for me cause I wanted it but I will remove it later since I already get a special icon but was feeling greedy and devious today. I am a menace to society.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ fetch("players.json")
 
       // Check for Early Participant status
       if(player.earlyP == true) {
-         out += `<img class="bird-logo" src="birdLogo.png" draggable="false">`;
+         out += `<img class="name-icon" src="birdLogo.png" draggable="false">`;
       }
       else {
          out += "";
@@ -26,7 +26,15 @@ fetch("players.json")
 
       // Check for Developer status
       if(player.developer == true) {
-         out += `<img class="dev-logo" src="TarynEnraged.png" draggable="false">`;
+         out += `<img class="name-icon" src="TarynEnraged.png" draggable="false">`;
+      }
+      else {
+         out += "";
+      }
+
+      // Check for M e n a c e status
+      if(player.menace == true) {
+         out += `<img class="name-icon" src="wuddleFace.png" draggable="false">`;
       }
       else {
          out += "";
