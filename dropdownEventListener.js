30bd19e0f0be820589ed9b9882addc6be0f59e55
Jonathan Hill <jwhill1452@gmail.comm>
2024-05-29 12:05:07 -0500

    Dropdown Menu Fr. Still doesn't link but will fix later.

--- dropdownEventListener.js
+++ dropdownEventListener.js
@@ -1,5 +1,10 @@
-function activateDropdownMenu() {
-    document.getElementById("dropdownMenuButton").addEventListener('click', ()=> {
+let menuOpen = false;
+document.getElementById("dropdownMenuButton").addEventListener('click', ()=> {
+    if (menuOpen == false) {
         document.getElementById("dropdownMenu").style.zIndex = 35;
-    });
-}
+        menuOpen = true;
+    } else if (menuOpen == true) {
+        document.getElementById("dropdownMenu").style.zIndex = -1;
+        menuOpen = false;
+    };
+});
