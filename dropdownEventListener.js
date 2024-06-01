240fbbe7068753dbdb1e637b018083977ab92f2a
Jonathan Hill <jwhill1452@gmail.comm>
2024-06-01 09:19:22 -0500

    Random garbage commits. Dev page work. The dropdown menu button. I forogt.

--- dropdownEventListener.js
+++ dropdownEventListener.js
@@ -1,4 +1,5 @@
 let menuOpen = false;
+if (document.getElementById("dropdownMenuButton")) {
 document.getElementById("dropdownMenuButton").addEventListener('click', ()=> {
     if (menuOpen == false) {
         document.getElementById("dropdownMenu").style.zIndex = 35;
@@ -7,7 +8,18 @@ document.getElementById("dropdownMenuButton").addEventListener('click', ()=> {
         document.getElementById("dropdownMenu").style.zIndex = -1;
         menuOpen = false;
     };
-});
+});}
+
+if (document.getElementById("dropdownMenuButtonDevPage")) {
+document.getElementById("dropdownMenuButtonDevPage").addEventListener('click', ()=> {
+    if (menuOpen == false) {
+        document.getElementById("dropdownMenu").style.zIndex = 35;
+        menuOpen = true;
+    } else if (menuOpen == true) {
+        document.getElementById("dropdownMenu").style.zIndex = -1;
+        menuOpen = false;
+    };
+});}
 
 document.getElementById("closeOption").addEventListener('click', ()=> {
     if (menuOpen == false) {
